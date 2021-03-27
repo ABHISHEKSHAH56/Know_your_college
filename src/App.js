45f4ff45df156d042ee6cc777c5ad147da508445
Abhishekshah5 <ak8756111@gmail.com>
2021-03-27 17:11:04 +0530

    'olokolko'

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FirstPage from './HOME/first'
 import Second from './HOME/sec'
 import Index from './HOME/third/index'
 import Cutoff from './HOME/third/View/cutoff/index'
+import Fees from './HOME/third/View/Fess'
 
 
 
@@ -17,6 +18,7 @@ export default function App() {
                                 <Route path='/' exact component={FirstPage} />
                                 <Route path='/second' component={Second} />
                                 <Route path='/overview/:slug' exact component={Index} />
+                                <Route path='/fees/:slug' exact component={Fees} />
                                 <Route path='/comingsoon' exact component={Checkout} />
                                 <Route path='x/cutoff/:slug' exact component={Cutoff} />                          
                                 
